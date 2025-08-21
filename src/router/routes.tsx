35aa63cf8d9ce78ac27af296a554d90f575ea18d
lyanan <lyanan09@163.com>
2025-08-21 19:32:58 +0100

    update for deployment

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const Home = lazy(() => import("../pages/home"));
 
 const AppRoutes = () => (
-  <Router>
+  <Router basename="/portfolio">
     <Routes>
       <Route
         path="/"
